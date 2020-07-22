7dd447df0ed92c297ee5406a47ea57be8a8d7a4c
omid-joukar <58634223+omid-joukar@users.noreply.github.com>
2020-07-22 08:36:40 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/**
+ * Created by KPS on 7/22/2020.
+ */
+public class Main {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input an integer: ");
+        int digits = in.nextInt();
+        System.out.println("The sum is " + sumDigits(digits));
+    }
+
+    public static int sumDigits(long n) {
+        int result = 0;
+
+        while(n > 0) {
+            result += n % 10;
+            n /= 10;
+        }
+
+        return result;
+    }
+}
